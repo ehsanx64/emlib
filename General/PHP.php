ba6576fdbf86b15635b75d645a4d0f0f3889fd37
Ehsan Mohammadi <ehsanx64@users.noreply.github.com>
2017-09-04 12:26:18 +0430

    Added error reporting configuration routines

--- General/PHP.php
+++ General/PHP.php
@@ -2,4 +2,19 @@
 namespace ehsanx64\libphp\General;
 
 class PHP {
+    /**
+	 * Enable displaying of all errors
+	 */
+	public static function enableFullErrorReporting() {
+		ini_set('display_errors', 1);
+		error_reporting(E_ALL);
+	}
+
+	/**
+	 * Enable displaying of only fatal errors
+	 */
+	public static function enableErrorReporting() {
+		ini_set('display_errors', 1);
+		error_reporting(E_ERROR);
+	}
 }
