e403e7b0be349c482eb0680320f70b1f77452c94
Ehsan Mohammadi <ehsanx64@gmail.com>
2017-09-17 20:57:20 +0430

    Method name in Numeral refactored

--- Persian/Numeral.php
+++ Persian/Numeral.php
@@ -5,14 +5,14 @@ include __DIR__ . '/jdf-2.70.php';
 
 class Numeral {
 	/**
-	 * @param $latinNumeral
-	 * @param string $to
-	 * @param string $dotReplacement
+	 * @param $latinNumeral Number string to convert
+	 * @param string $to Language code to convert numeral to (en or fa)
+	 * @param string $dotReplacement Should function convert thousand separator too
 	 *
 	 * Convert between Persian\Latin numerals. Stolen from jdf :-D
 	 * @return mixed
 	 */
-	public static function latinToPersian($numeral, $to = 'en', $dotReplacement = '،') {
+	public static function convertNumeral($numeral, $to = 'en', $dotReplacement = '،') {
 		$num_a = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.');
 		$key_a = array('۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹', $dotReplacement);
 
