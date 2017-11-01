99f69f62d73b2d82cad43c6ab498d6848605ed2f
Ehsan Mohammadi <ehsanx64@users.noreply.github.com>
2017-11-01 17:42:57 +0330

    Update Date.php

--- Persian/Date.php
+++ Persian/Date.php
@@ -1,6 +1,4 @@
 <?php
-
-
 namespace ehsanx64\libphp\Persian;
 
 include __DIR__ . '/jdf-2.70.php';
@@ -10,30 +8,26 @@ class Date {
 	public static $standardDateFormat = 'j F Y';
 	public static $shortDateFormat = 'Y-m-d';
 
-	public static function getStandardDate($timestamp) {
+	
+	/*
+	 * Get current Jalali date based on the class standard format
+	 *
+	 * @param $timestamp mixed Optional. timestamp for date generation. Current timestamp will be used if not specified.
+	 *
+	 * @return string Date string
+	 */
+	public static function getStandardDate($timestamp = time()) {
 		echo Jdate::jdate(self::$standardDateFormat, $timestamp);
 	}
-	
-	/**
-	 * Convert Gregorian standard date (1985-12-01) to unix timestamp
-	 *
-	 * @param $gregorianDate Date string in format (1985-12-01)
-	 * @return int Unix timestamp casted as an integer
-	 */
-	public static function gregorianDateToTimestamp($gregorianDate) {
-		$p = explode('-', $gregorianDate);
 
-		if (count($p) == 3) {
-			return (int) mktime(null, null, null, $p[1], $p[2], $p[0]);
-		}
-
-		return '';
-	}
 	
 	/**
 	 * Convert Gregorian standard date (1985-12-01) into Jalali standard date (1364-12-22)
+	 *
+	 * @param $gregorianDateString mixed The Gregorian date for conversion
+	 * @return string Jalali date
 	 */
-	public static function gregorianDateToJalaliDate($gregorianDateString) {
-		return Jdate::jdate(self::$shortDateFormat, self::gregorianDateToTimestamp($gregorianDateString));
+	public static function ToJalali($gregorianDateString) {
+		return Jdate::jdate(self::$shortDateFormat, \ehsanx64\libphp\General\Date\Date::toTimestamp($gregorianDateString));
 	}
 }
