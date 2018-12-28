7bcaa4dc4974ef0e284ebc1757f825ee4209d02f
Ehsan Mohammadi <ehsanx64@gmail.com>
2018-12-28 11:35:36 +0330

    FileSystem class commented

--- FileSystem.php
+++ FileSystem.php
@@ -5,12 +5,22 @@ class FileSystem {
 	/**
 	 * Return array of directories in a given directory
 	 * @param $basePath The directory to return its subdirectories
+	 * @return array Array of sub-directories paths
 	 */
 	public function getDirectories($basePath) {
 		$dirs = array_filter(glob($basePath . DIRECTORY_SEPARATOR . '*'), 'is_dir');
 		return $dirs;
 	}
 
+	/**
+	 * Get the last component in a given path. For example if given path is:
+	 *
+	 * <i>/tmp/test/a</i>, Method will return a.</i>
+	 *
+	 * <b>Note:</b> This method does not care if the the last component of the path is a directory or file.
+	 * @param string $path A URI
+	 * @returns string Last component in the given path
+	 */
 	public function getLastPart($path) {
 		$parts = explode(DIRECTORY_SEPARATOR, $path);
 		return $parts[count($parts) - 1];
